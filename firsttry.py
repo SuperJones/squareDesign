62be0d6f2a57433586fdea798ad38896c264e4d7
Anissa Jones <anissajones10@gmail.com>
2015-06-17 17:44:58 -0400

    Discovered the goto function in the python libraries, by reviewing previous exercises and looking it up on the python website. Also realized I needed to create two functions and they both needed to have for loops in them to work.

--- firsttry.py
+++ firsttry.py
@@ -5,13 +5,14 @@ def drawSquare(t, sz):
         t.forward(sz)
         t.left(90)
         
-def Instruct(t, sz, axis):
-    drawSquare(t, sz)
-    t.penup()
-    t.goto(axis, axis)
-    t.pendown()
-    sz = sz + 20
-    axis = axis -10
+def Instruct(t, sz, axis, iter):
+    for j in range(iter):
+        drawSquare(t, sz)
+        t.penup()
+        t.goto(axis, axis)
+        t.pendown()
+        sz = sz + 20
+        axis = axis -10
     
     
         
@@ -22,6 +23,6 @@ alex = turtle.Turtle()
 alex.color("hotpink")
 
 
-Insrtuct(alex, 20, -10.0)
+Instruct(alex, 20, -10.0, 6)
 
 wn.exitonclick()
