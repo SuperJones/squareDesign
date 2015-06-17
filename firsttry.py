0b4bdfdc7b301f817d31eedf984e4395d7d0930c
Anissa Jones <anissajones10@gmail.com>
2015-06-17 17:23:18 -0400

    This is the 5th try actually but the first time I got frustrated and decided to save it, take a break and come back.

---
+++ firsttry.py
@@ -0,0 +1,22 @@
+def drawSquare(t, sz):
+    for i in range(4):
+        t.forward(sz)
+        t.left(90)
+ 
+wn = turtle.Screen()
+wn.bgcolor("lightgreen")
+ 
+alex = turtle.Turtle()
+alex.color("hotpink")
+ 
+sideLen = 20
+for i in range(6):
+    drawSquare(alex,sideLen)
+    alex.penup()
+    alex.backward(10)
+    alex.left(90)
+    alex.forward(10)
+    alex.pendown()
+    sideLen = sideLen + 20
+ 
+wn.exitonclick()
