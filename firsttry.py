d43e80e0b06abf4fa0b5a9bd30f9ebb2bd4376bd
Anissa Jones <anissajones10@gmail.com>
2015-06-17 17:37:04 -0400

    Have to figure out why the squares are getting bigger as the iterations go.

--- firsttry.py
+++ firsttry.py
@@ -1,24 +1,30 @@
-import turtle 
-
+import turtle
+ 
 def drawSquare(t, sz):
     for i in range(4):
         t.forward(sz)
         t.left(90)
  
+def drawFour(t, sz, scoot):
+    for i in range(6):
+        drawSquare(t, sz)
+		#put penup in comments so that I could see the line being drawn and 
+		#try to figure out what is going wrong.
+        #t.penup()  
+        t.backward(scoot)
+        t.right(90)
+        t.forward(scoot)
+        t.left(90)
+        #t.pendown()
+        sz = sz + sz
+        scoot = scoot + scoot
+       
 wn = turtle.Screen()
 wn.bgcolor("lightgreen")
  
 alex = turtle.Turtle()
 alex.color("hotpink")
  
-sideLen = 20
-for i in range(6):
-    drawSquare(alex,sideLen)
-    alex.penup()
-    alex.backward(10)
-    alex.left(90)
-    alex.forward(10)
-    alex.pendown()
-    sideLen = sideLen + 20
+drawFour(alex, 20, 10)
  
 wn.exitonclick()
